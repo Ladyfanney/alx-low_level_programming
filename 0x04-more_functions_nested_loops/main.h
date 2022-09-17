9c97402e64ac704321c515d45eb033f9ac4cbddc
fanney <fanneyokato@gmail.com>
2022-09-16 20:51:41 -0700

    Main header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifnedf MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -16,4 +16,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /*MAIN_H*/
+#endif /* MAIN_H */
