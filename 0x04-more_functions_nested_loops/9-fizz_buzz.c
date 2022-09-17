72a14eb0918532b5a9c46b44d604a138d442b088
fanney <fanneyokato@gmail.com>
2022-09-17 09:15:06 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,10 +2,10 @@
 
 /**
  * main - print numbers 1 - 100 followed by a new line
- * 	numbers that are multiples of 3 print Fizz
- * 	numbers that are multiples of 5 print Buzz
- * 	numbers that are multiples of 3 n 5 print FizzBuzz
- * 	each number and word to be separated by space
+ * numbers that are multiples of 3 print Fizz
+ * numbers that are multiples of 5 print Buzz
+ * numbers that are multiples of 3 n 5 print FizzBuzz
+ * each number and word to be separated by space
  *
  * Return: Always 0 (Success)
 */
@@ -31,5 +31,5 @@ int main(void)
 			printf("\n");
 	}
 
-	return (0)
+	return (0);
 }
