ab21ac882ac55bc629a7d1f5f17e89d430f0e6f7
fanney <fanneyokato@gmail.com>
2022-10-04 19:34:30 -0700

    A function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -28,7 +28,7 @@ char *_strdup(char *str)
 
 	while (str[i] != '\0')
 	{
-		s[i] = s[i];
+		s[i] = str[i];
 		i++;
 	}
 
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - this is a function that concatenates two strings
+ * @s1: input to string 1
+ * @s2: input to string 2
+ * Return: NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a = 0, b = 0;
